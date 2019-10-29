7db1e7fafaf67174863d04e16a0fdc2a54cae95f
Eric Davenport <EricDavenport@pursuit.org>
2019-10-29 17:34:32 -0400

    repaired card flip - keeping count - need to add 4-card game

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,62 +10,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var card1: UIButton!
+    
+    @IBOutlet weak var card2: UIButton!
+    
+    @IBOutlet weak var card3: UIButton!
+    
     @IBOutlet weak var winLoseMessage: UILabel!
     
     var wins: Int = 0
     
     let imageArray = ["threeCard", "kingCard", "threeCard"]
     
+    
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func newGame(_ sender: UIButton) {
-        
-    }
+
     
     
     
 
     @IBAction func flipCard(_ sender: UIButton) {
         
-        sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
+       let buttonArray = [card1, card2, card3]
+        
+         let randomImage = sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
+        
         
         let randomNumber = Int.random(in: 0...2)
-//        let tag = sender.tag
-//            switch tag {
-//            case 1:
-//                sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
-//            case 2:
-//                sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
-//            case 3:
-//                sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
-//            case 4:
-//                sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
-//            case 5:
-//                sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
-//            default:
-//                sender.setBackgroundImage(UIImage(named:"\(imageArray.randomElement() ?? "threeCard")"), for: UIControl.State.normal)
-//            }
-        
+
         if sender.tag == randomNumber {
              sender.setBackgroundImage(UIImage(named:"kingCard"), for: UIControl.State.normal)
             wins += 1
-            winLoseMessage.text = "YOU WIN!!! \(wins) wins"
+            winLoseMessage.text = "YOU WIN!!!\n \(wins) wins"
+            
+            for button in buttonArray {
+                button?.isEnabled = false
+            }
             
         } else {
             sender.setBackgroundImage(UIImage(named: "threeCard"), for: UIControl.State.normal)
             winLoseMessage.text = "YOU LOSE!!!"
+            
+            for button in buttonArray {
+                button?.isEnabled = false
+            }
         }
         
+        }
+
+    @IBAction func newGame(_ sender: Any) {
+       
+        let buttonArray = [card1, card2, card3]
         
-            
+        card1.setBackgroundImage(UIImage(named: "cardBackRed"), for: UIControl.State.normal)
+        card2.setBackgroundImage(UIImage(named: "cardBackRed"), for: UIControl.State.normal)
+        card3.setBackgroundImage(UIImage(named: "cardBackRed"), for: UIControl.State.normal)
         
-        
-        
+        for button in buttonArray {
+            button?.isEnabled = true
+        }
     }
-    
 }
 
